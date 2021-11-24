fda8e6bed5b5760862f5c1066e073b739c4bab07
ArtturiP <89945435+ArtturiP@users.noreply.github.com>
2021-11-24 16:09:45 +0200

    Update BudgetController.java

--- BudgetCalculator/src/main/java/com/example/BudgetCalculator/web/BudgetController.java
+++ BudgetCalculator/src/main/java/com/example/BudgetCalculator/web/BudgetController.java
@@ -28,70 +28,84 @@ public class BudgetController {
 	@Autowired
 	private PlannedRepository prepository;
 	
-	private List<Budget> budgets = new ArrayList<Budget>();
-	@RequestMapping(value = {"/", "/index"}, method = RequestMethod.GET)
-	public String home(@RequestParam(name = "budget", required = false)String name, Model model) {
-
-		budgets = new ArrayList<Budget>();
-		Budget budget = new Budget();
-		model.addAttribute("budget", budget);
+	// kotisivu
+	@RequestMapping(value = {"/", "/index"})
+	public String home () {
 		return "index";
 	}
-	
 
-	
-	@RequestMapping(value="/budgetlist")
+	//lista budjeteista
+	@RequestMapping(value="/budgets")
 	public String budgetList(Model model) {
+	//haetaan menneistä budjetit
 	model.addAttribute("budgets", repository.findAll());
+	//haetaan suunnitellut budjetit
 	model.addAttribute("plannedbudgets", prepository.findAll());
-	
-	return "budgetlist";
+	return "budgets";
 	}
 	
+	//lisätään menneitä budjetteja
 	@RequestMapping(value = "/add")
 	public String addBudget(Model model){
+	 //lisätään budjetti listaan
 	 model.addAttribute("budget", new Budget());
 	 return "addbudget";
 	}
 	
+	//lisätään budjettisuunnitelmia
 	@RequestMapping(value = "/addplan")
 	public String addPlannedBudget(Model model){
+	 //lisätään budjetti listaan
 	 model.addAttribute("plannedbudget", new PlannedBudget());
 	 return "addplannedbudget";
 	}
 	
+	//menneiden budjettien tallennus
 	@RequestMapping(value = "/save", method = RequestMethod.POST)
 	public String save(Budget budget){
 	 repository.save(budget);
-	 return "redirect:budgetlist";
+	 //siirrytään takaisin budjettilistaukseen
+	 return "redirect:budgets";
 	}
 	
+	//budjettisuunnitelmien tallennus
 	@RequestMapping(value = "/saveplan", method = RequestMethod.POST)
 	public String save(PlannedBudget plannedbudget){
 	 prepository.save(plannedbudget);
-	 return "redirect:budgetlist";
+	 //siirrytään takaisin budjettilistaukseen
+	 return "redirect:budgets";
 	}
 	
+	//menneiden budjettien poisto
 	@RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
 	public String deleteBudget(@PathVariable("id") Long budgetId, Model model) {
+	 //poistetaan id:n perusteella
 	 repository.deleteById(budgetId);
-	 return "redirect:../budgetlist";
+	//siirrytään takaisin budjettilistaukseen
+	 return "redirect:../budgets";
 	}
 	
+	//budjettisuunnitelmien poisto
 	@RequestMapping(value = "/deleteplan/{id}", method = RequestMethod.GET)
 	public String deletePlannedBudget(@PathVariable("id") Long plannedbudgetId, Model model) {
+	 //poistetaan id:n perusteella
 	 prepository.deleteById(plannedbudgetId);
-	 return "redirect:../budgetlist";
+	//siirrytään takaisin budjettilistaukseen
+	 return "redirect:../budgets";
 	}
 	
+	//menneiden budjettien muokkaus
 	@RequestMapping(value = "/edit/{id}")
 	public String addBudget(@PathVariable("id") Long budgetId, Model model){
+	 //tallennetaan tiedot id:n perusteella
 	 model.addAttribute("budget", repository.findById(budgetId));
 	 return "editbudget";
 	}
 	
+	//budjettisuunnitelmien muokkaus
 	@RequestMapping(value = "/editplan/{id}")
 	public String addPlannedBudget(@PathVariable("id") Long plannedbudgetId, Model model){
+	 //tallennetaan tiedot id:n perusteella
 	 model.addAttribute("plannedbudget", prepository.findById(plannedbudgetId));
 	 return "editplannedbudget";
 	}
