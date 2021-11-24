c3f65f7a8fcd6a5212e39cabcfbe5eed2fde9af4
ArtturiP <89945435+ArtturiP@users.noreply.github.com>
2021-11-24 16:08:34 +0200

    Update BudgetRepository.java

--- BudgetCalculator/src/main/java/com/example/BudgetCalculator/domain/BudgetRepository.java
+++ BudgetCalculator/src/main/java/com/example/BudgetCalculator/domain/BudgetRepository.java
@@ -2,7 +2,7 @@ package com.example.BudgetCalculator.domain;
 
 import org.springframework.data.repository.CrudRepository;
 
-
+//repo menneille budjeteille
 public interface BudgetRepository extends CrudRepository<Budget, Long> {
 
 }
