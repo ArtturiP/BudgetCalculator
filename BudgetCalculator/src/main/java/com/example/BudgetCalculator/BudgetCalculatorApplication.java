d2315a34162f0a91822adce3db7e7fdc1d73daee
ArtturiP <89945435+ArtturiP@users.noreply.github.com>
2021-11-24 16:07:53 +0200

    Update BudgetCalculatorApplication.java

--- BudgetCalculator/src/main/java/com/example/BudgetCalculator/BudgetCalculatorApplication.java
+++ BudgetCalculator/src/main/java/com/example/BudgetCalculator/BudgetCalculatorApplication.java
@@ -1,13 +1,25 @@
 package com.example.BudgetCalculator;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
+import com.example.BudgetCalculator.domain.BudgetRepository;
+import com.example.BudgetCalculator.domain.PlannedRepository;
+
+//budjettilaskurin sovellus
 @SpringBootApplication
 public class BudgetCalculatorApplication {
-
+	
 	public static void main(String[] args) {
 		SpringApplication.run(BudgetCalculatorApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner bookDemo(BudgetRepository repository, PlannedRepository prepository) {
+		return (args) -> {
+			
+		};
+	}
 }
