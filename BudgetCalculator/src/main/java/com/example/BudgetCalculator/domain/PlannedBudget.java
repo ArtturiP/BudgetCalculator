e763f7c06345b83ecbe145aed31b410e500f0ec4
ArtturiP <89945435+ArtturiP@users.noreply.github.com>
2021-11-24 16:08:58 +0200

    Update PlannedBudget.java

--- BudgetCalculator/src/main/java/com/example/BudgetCalculator/domain/PlannedBudget.java
+++ BudgetCalculator/src/main/java/com/example/BudgetCalculator/domain/PlannedBudget.java
@@ -7,51 +7,124 @@ import javax.persistence.Id;
 
 @Entity
 public class PlannedBudget {
+	
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
-	private String month;
-	private int income;
-	private int spend;
+	private String month; //budjetin kuukausi
+	private int salary; //palkka
+	private int otherinc; //muut tulot
+	private int income; //tulot yhteensä
+	private int rent; //vuokra
+	private int bills; //laskut
+	private int food; //ruoka
+	private int essentials; //välttämättömät menot
+	private int entertainment; //viihde
+	private int otherspe; //muut menot
+	private int spend; //menot yhteensä
 	
+	//getterit ja setterit
 	public Long getId() {
 		return id;
 	}
 	public void setId(Long id) {
 		this.id = id;
 	}
-	public int getIncome() {
-		return income;
-	}
-	public void setIncome(int income) {
-		this.income = income;
-	}
-	public int getSpend() {
-		return spend;
-	}
-	public void setSpend(int spend) {
-		this.spend = spend;
-	}	
-	
 	public String getMonth() {
 		return month;
 	}
 	public void setMonth(String month) {
 		this.month = month;
 	}
-	public PlannedBudget() {
+	public int getSalary() {
+		return salary;
+	}
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+	public int getOtherinc() {
+		return otherinc;
+	}
+	public void setOtherinc(int otherinc) {
+		this.otherinc = otherinc;
+	}
+	public int getIncome() {
+		return salary + otherinc;
+	}
+	public void setIncome(int income) {
+		this.income = salary + otherinc;
+	}
+	public int getRent() {
+		return rent;
+	}
+	public void setRent(int rent) {
+		this.rent = rent;
+	}
+	public int getBills() {
+		return bills;
+	}
+	public void setBills(int bills) {
+		this.bills = bills;
+	}
+	public int getFood() {
+		return food;
+	}
+	public void setFood(int food) {
+		this.food = food;
+	}
+	public int getEssentials() {
+		return essentials;
+	}
+	public void setEssentials(int essentials) {
+		this.essentials = essentials;
+	}
+	public int getEntertainment() {
+		return entertainment;
+	}
+	public void setEntertainment(int entertainment) {
+		this.entertainment = entertainment;
+	}
+	public int getOtherspe() {
+		return otherspe;
+	}
+	public void setOtherspe(int otherspe) {
+		this.otherspe = otherspe;
+	}
+	public int getSpend() {
+		return rent + bills + food + essentials + entertainment + otherspe;
+	}
+	public void setSpend(int spend) {
+		this.spend = rent + bills + food + essentials + entertainment + otherspe;
 	}
 	
-	public PlannedBudget(String month, int income, int spend) {
+	//konstruktorit
+	public PlannedBudget() {
+		super();
+	}
+	
+	public PlannedBudget(String month, int salary, int otherinc, int income, int rent, int bills, int food,
+			int essentials, int entertainment, int otherspe, int spend) {
 		super();
 		this.month = month;
+		this.salary = salary;
+		this.otherinc = otherinc;
 		this.income = income;
+		this.rent = rent;
+		this.bills = bills;
+		this.food = food;
+		this.essentials = essentials;
+		this.entertainment = entertainment;
+		this.otherspe = otherspe;
 		this.spend = spend;
 	}
 	
 	@Override
 	public String toString() {
-		return "PlannedBudget [id=" + id + ", month=" + month + ", income=" + income + ", spend=" + spend + "]";
+		return "Budget [id=" + id + ", month=" + month + ", salary=" + salary + ", otherinc=" + otherinc + ", income="
+				+ income + ", rent=" + rent + ", bills=" + bills + ", food=" + food + ", essentials=" + essentials
+				+ ", entertainment=" + entertainment + ", otherspe=" + otherspe + ", spend=" + spend + "]";
 	}
-
+	
+	
+	
 }
